cbb294b8a9672ebe4fd2e16a71b047dbc6bd5fae
OfficialKris <37947442+OfficialKris@users.noreply.github.com>
2023-04-03 13:35:29 -0700

    Added test echo

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	app := "echo"
+	arg0 := "-e"
+	arg1 := "Hellow World"
+	arg2 := "\n\tfrom"
+	arg3 := "golang"
+
+	cmd := exec.Command(app, arg0, arg1, arg2, arg3)
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(string(stdout))
+}
