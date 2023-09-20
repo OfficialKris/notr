31db27d9afd46955a07485879358114535065146
OfficialKris <37947442+OfficialKris@users.noreply.github.com>
2023-09-20 09:39:18 -0700

    Added initial config files

---
+++ config/config_dev.go
@@ -0,0 +1,10 @@
+//go:build dev
+
+package config
+
+const (
+	DEV_MODE         = true
+	PROGRAM_VERSION  = "0.0.1a"
+	CONFIG_LOCATION  = "/.config/notr-dev/"
+	CONFIG_FILE_NAME = "config.json"
+)
---
+++ config/config_prod.go
@@ -0,0 +1,10 @@
+//go:build prod
+
+package config
+
+const (
+	DEV_MODE         = false
+	PROGRAM_VERSION  = "0.0.1a"
+	CONFIG_LOCATION  = "/.config/notr/"
+	CONFIG_FILE_NAME = "config.json"
+)
